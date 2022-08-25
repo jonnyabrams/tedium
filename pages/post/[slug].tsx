756cdc7093eb678d66322e30a1d42ceef164d997
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-25 16:52:09 +0200

    feat: getStaticProps to populate each page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,8 +1,13 @@
+import { GetStaticProps } from "next";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
 
-const PostPage = () => {
+interface Props {
+  post: Post;
+}
+
+const PostPage = ({ post }: Props) => {
   return (
     <main>
       <Header />
@@ -20,6 +25,7 @@ export const getStaticPaths = async () => {
 
   const posts = await sanityClient.fetch(query);
 
+  // get array of paths with all the slugs we need
   const paths = posts.map((post: Post) => ({
     params: {
       slug: post.slug.current,
@@ -29,5 +35,44 @@ export const getStaticPaths = async () => {
   return {
     paths,
     fallback: "blocking",
+  };
+};
+
+// use those slugs to get the information for each page
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    author-> {
+    name,
+    image
+    },
+    'comments': *[
+      _type == "comment" &&
+      post._ref == ^._id &&
+      approved == true],
+    description,
+    mainImage,
+    slug,
+    body
+  }`;
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+
+  // if not post found, return a 404 page
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  // if post found, return it as a prop
+  return {
+    props: {
+      post,
+    },
   };
 };
