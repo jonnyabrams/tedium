8d537537875d169583cae647044fd3481e298151
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-25 12:09:09 +0200

    styles: 1, 2 or 3 posts in column depending on screen size

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Posts = ({ posts }: Props) => {
   return (
-    <div>
+    <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 lg:grid-cols-3 md:gap-6 md:p-6">
       {posts.map((post) => (
         <PostComponent post={post} key={post._id} />
       ))}
